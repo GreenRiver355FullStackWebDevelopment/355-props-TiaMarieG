724ac852368b3b27997ec3d228e266dfea4acf56
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-04-08 19:10:11 -0700

    Completed step five of acessing props to return displayed information.

--- props-and-events/src/components/Restaurant.jsx
+++ props-and-events/src/components/Restaurant.jsx
@@ -1,9 +1,17 @@
-// JS file that holds a function which returns a div
-function Restaurant() {
+// JSX file that holds a function which returns a div
+function Restaurant(props) {
   return (
-      <div className="restaurant">
-
-      </div>
+    // Added a props parameter to gather info passed from parent - RC.jsx
+    <div className="restaurant">
+      <h2>{props.restaurant.name}</h2>
+      <img src={props.restaurant.image} alt={props.name} />
+      <ul>
+        <li>Address: {props.restaurant.address}</li>
+        <li>Phone Number: {props.restaurant.phone}</li>
+        <li>Cuisine: {props.restaurant.cuisine}</li>
+        <li>Rating: {props.restaurant.rating}</li>
+      </ul>
+    </div>
   );
 }
 
--- props-and-events/src/components/RestaurantsContainer.jsx
+++ props-and-events/src/components/RestaurantsContainer.jsx
@@ -1,4 +1,4 @@
-// JS file that holds a function which returns a div.
+// JSX file that holds a function which returns a div.
 
 // Importing Restaurant.js to RC.js
 import Restaurant from "./Restaurant.jsx";
@@ -10,8 +10,10 @@ function RestaurantsContainer() {
   return (
     // This part really confused me. I had to look at the solution to figure it out
     // Still don't fully understand it.
+    // Added a key as the console was giving an error if there wasn't one.
+    // I'm guessing it's because there is an id listed in the array
     <div className="restaurantContainer"> 
-      {restaurants.map(restaurant => <Restaurant restaurant={restaurant} />)}
+      {restaurants.map(restaurant => <Restaurant restaurant={restaurant} key={restaurant.id}/>)}
     </div>
   ); 
 }
